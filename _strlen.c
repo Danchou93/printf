81f2498803193a296b91c8155b043dc7b91d950a
Abdulrahmanabuziad <budiielsayed@gmail.com>
2023-09-19 11:49:02 -0700

    _strlen.c

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @str: the string
+ * Return: the length
+ */
+
+int _strlen(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+		;
+	return (0);
+}
